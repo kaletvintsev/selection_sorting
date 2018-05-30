56c8941f4ca8b2061ad365a581d512783f3ff61d
kaletvintsev <kaletvintsev@gmail.com>
2018-05-30 18:11:25 +0300

    Add search

--- selection_search.py
+++ selection_search.py
@@ -1,1 +1,28 @@
- 
+def search_small(arr):
+	
+	small = arr[0]
+	small_index = 0
+
+	for i in range(len(arr)):
+
+		if arr[i] < small:
+
+			small = arr[i]
+			small_index = i
+
+	return small_index
+
+
+def selection_search(arr):
+
+	for i in range(len(arr)):
+
+		small_index = search_small(arr)
+
+		changed_val = arr[i]
+		arr[i] = arr[small_index]
+		arr[small_index] = changed_val
+
+	return arr
+
+	
